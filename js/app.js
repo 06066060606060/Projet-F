1f30d0b153253416089290e1dd88ef04f4dc6b11
06066060606060 <xmicky+git@hotmail.fr>
2022-08-27 01:47:05 +0400

    slider ok

--- js/app.js
+++ js/app.js
@@ -3,23 +3,27 @@ slider.defaultValue = 0
 const react1 = document.querySelector('#react1')
 const react2 = document.querySelector('#react2')
 const react3 = document.querySelector('#react3')
-
+ 
 function changeHandler (e) {
+    
     switch(parseInt(e.value)) {
         case 0:
         react1.style.display = 'block'
         react2.style.display = 'none'
         react3.style.display = 'none'
+        slider.style.background = ''
         break;
         case 1: 
         react2.style.display = 'block'
         react1.style.display = 'none'
         react3.style.display = 'none'
-        break;  
+        slider.style.background = 'linear-gradient(to right, #0D59EF 0%, #0D59EF 50%, white 50%, white 100%)'
+        break;
         case 2: 
         react3.style.display = 'block'
         react1.style.display = 'none'
         react2.style.display = 'none'
+        slider.style.background = 'linear-gradient(to right, #0D59EF 0%, #0D59EF, #0D59EF 100%)'
         break;
         default: 
         react1.style.display = 'block'
