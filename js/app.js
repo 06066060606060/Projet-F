35b8525ebd342fb79ab00974b962054509e69ccb
06066060606060 <xmicky+git@hotmail.fr>
2022-08-27 02:49:11 +0400

    menu mobile

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const slider = document.getElementById('slider')
+const client1 = document.getElementById('client1')
+const client2 = document.getElementById('client2')
 slider.defaultValue = 0
 const react1 = document.querySelector('#react1')
 const react2 = document.querySelector('#react2')
@@ -34,4 +36,18 @@ function changeHandler (e) {
         break;
     
     }
+}
+
+function friend1() {
+
+    client1.style.display = 'none'
+    client2.style.display = 'flex'
+
+}
+
+function friend2() {
+
+    client2.style.display = 'none'
+    client1.style.display = 'flex'
+
 }
