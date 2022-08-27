383331e6890c53f96658a46a9c32ea2f04470ca8
06066060606060 <xmicky+git@hotmail.fr>
2022-08-27 15:33:33 +0400

    slider value ok

--- js/app.js
+++ js/app.js
@@ -5,49 +5,35 @@ slider.defaultValue = 0
 const react1 = document.querySelector('#react1')
 const react2 = document.querySelector('#react2')
 const react3 = document.querySelector('#react3')
+const valueslider = document.getElementById('textInput')
  
 function changeHandler (e) {
-    
-    switch(parseInt(e.value)) {
-        case 0:
+    console.log(valueslider.value)
+    switch(true) {
+        case (0 <= valueslider.value &&  valueslider.value < 33):
         react1.style.display = 'block'
         react2.style.display = 'none'
         react3.style.display = 'none'
-        slider.style.background = ''
         break;
-        case 1: 
+        case (34 <= valueslider.value &&  valueslider.value < 66):
         react2.style.display = 'block'
         react1.style.display = 'none'
         react3.style.display = 'none'
-        slider.style.background = 'linear-gradient(to right, #0D59EF 0%, #0D59EF 50%, white 50%, white 100%)'
         break;
-        case 2: 
+        case (67 <= valueslider.value &&  valueslider.value < 100):
         react3.style.display = 'block'
         react1.style.display = 'none'
         react2.style.display = 'none'
-        slider.style.background = 'linear-gradient(to right, #0D59EF 0%, #0D59EF, #0D59EF 100%)'
         break;
-        default: 
-        react1.style.display = 'block'
-        react3.style.display = 'none'
-        react2.style.display = 'none'
-        
-     
-        break;
-    
     }
 }
 
-function friend1() {
+function updateTextInput(val) {
+    const color_1 = 'linear-gradient(to right, #0D59EF 0%, #0D59EF '+ val +'%, white '+ val +'%, white 100%)'
+    const color_2 = '#FFFFFF'
+    document.getElementById('textInput').value=val; 
+    slider.style.background = color_1;
 
-    client1.style.display = 'none'
-    client2.style.display = 'flex'
 
-}
-
-function friend2() {
-
-    client2.style.display = 'none'
-    client1.style.display = 'flex'
-
-}
+ 
+  }
