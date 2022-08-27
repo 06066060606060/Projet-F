5e720d819533840ecff3840fbd62ab41fb15c837
06066060606060 <xmicky+git@hotmail.fr>
2022-08-27 18:38:05 +0400

    fix slider

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 const slider = document.getElementById('slider')
 const client1 = document.getElementById('client1')
 const client2 = document.getElementById('client2')
-slider.defaultValue = 0
+slider.defaultValue = 33
 const react1 = document.querySelector('#react1')
 const react2 = document.querySelector('#react2')
 const react3 = document.querySelector('#react3')
 const valueslider = document.getElementById('textInput')
+const textInputr =  document.getElementById('textInputr')
+
  
 function changeHandler (e) {
     console.log(valueslider.value)
@@ -24,15 +26,24 @@ function changeHandler (e) {
         react3.style.display = 'block'
         react1.style.display = 'none'
         react2.style.display = 'none'
+
         break;
     }
 }
 
 function updateTextInput(val) {
-    const color_1 = 'linear-gradient(to right, #0D59EF 0%, #0D59EF '+ val +'%, white '+ val +'%, white 100%)'
+    const color_1 = 'linear-gradient(to right, #0D59EF 33%, #0D59EF '+ val +'%, white '+ val +'%, white 100%)'
     const color_2 = '#FFFFFF'
-    document.getElementById('textInput').value=val; 
-    slider.style.background = color_1;
+    var rounded = Math.ceil(val / 33)
+    document.getElementById('textInputr').value=rounded;
+    document.getElementById('textInput').value=val;
+    textInputr.style.marginLeft = val-2 + '%';
+    if (val < 1) {
+        slider.style.background = color_2;
+    } else {
+        slider.style.background = color_1;
+    }
+  
 
 
  
